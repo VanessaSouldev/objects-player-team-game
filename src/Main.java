3255a6e9853595d58799aaad6dcfd140f9ec60a8
VanessaSouldev <vanessasouldev@gmail.com>
2023-10-06 11:46:49 +1100

    done

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,27 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Team t1 = new Team("Banaan", "Amsterdam");
-
         Player p1 = new Player("Leo Messi", 10_000_000, 'm');
-
         Player p2 = new Player("Lieke Martens", 20_000_000, 'f');
+
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(p1);
+        players.add(p2);
+
+        Team t1 = new Team("RealMadrid", "Madrid");
+        Team t2 = new Team("Ajax", "Amsterdam");
+        Game g1 = new Game(t1, t2);
+
+        g1.addGoal(true);
+        g1.addGoal(true);
+
 
         t1.addPlayer(p1);
         t1.addPlayer(p2);
-        t1.showPlayers();
+
+        g1.print();
+
+
+
     }
 }
